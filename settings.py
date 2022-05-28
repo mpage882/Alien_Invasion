b6f77e2496a294b4a2c0ac4d1f164599ffea9e51
Marilyn Page <mpage882@gmail.com>
2022-05-28 13:34:49 -0500

    commit

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 39)
+        self.bg_color = (0, 0, 65, 1)
 
         # ship settings
         self.ship_speed = 1.5
